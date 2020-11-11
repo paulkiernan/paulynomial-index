47680969e0416cba9a25d868b06630558987df7a
Paul Kiernan <paul.kiernan@reddit.com>
2020-11-11 00:29:29 -0800

    Containerizes static application

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -10,6 +10,7 @@ docker run \
     -e RUN_LOCAL=true \
     -e VALIDATE_ALL_CODEBASE=true \
     -e VALIDATE_CSS=true \
+    -e VALIDATE_DOCKERFILE=true \
     -e VALIDATE_HTML=true \
     -e VALIDATE_JAVASCRIPT_STANDARD=true \
     -e VALIDATE_JSON=true \
