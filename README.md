7b41ef6fee9614c9f87ca71829b54cf5cf5173a7
Paul Kiernan <paul.kiernan@reddit.com>
2020-11-10 22:14:16 -0800

    Adds linter

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# paulynomial-index
-Splash page for personal website
+# Paulynomial Index
+
+![Super-Linter](https://github.com/paulkiernan/paulynomial-index/workflows/Super-Linter/badge.svg)
+
+Splash page for personal website. Superimposes links for other services and
+projects over a canvas drawing of a simplified bubble chamber.
---
+++ scripts/lint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -eu
+
+REPO_ROOT_DIR=$(git rev-parse --show-toplevel)
+
+docker run \
+    --rm \
+    -e OUTPUT_DETAILS=detailed \
+    -e RUN_LOCAL=true \
+    -e VALIDATE_ALL_CODEBASE=true \
+    -e VALIDATE_JAVASCRIPT_STANDARD=true \
+    -e VALIDATE_JSON=true \
+    -v "$REPO_ROOT_DIR":/tmp/lint \
+    github/super-linter
