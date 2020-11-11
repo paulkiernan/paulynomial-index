677bfc0732145827285b1309a19558bcdc5fe125
Paul Kiernan <paul.kiernan@reddit.com>
2020-11-10 23:36:58 -0800

    Updates linter to include CSS and HTML linting

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -9,6 +9,8 @@ docker run \
     -e OUTPUT_DETAILS=detailed \
     -e RUN_LOCAL=true \
     -e VALIDATE_ALL_CODEBASE=true \
+    -e VALIDATE_CSS=true \
+    -e VALIDATE_HTML=true \
     -e VALIDATE_JAVASCRIPT_STANDARD=true \
     -e VALIDATE_JSON=true \
     -v "$REPO_ROOT_DIR":/tmp/lint \
